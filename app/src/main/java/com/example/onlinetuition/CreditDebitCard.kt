c52c0f961a6dfe20cbba37a4bf934b43c3f1ab5d
ChongSingMeng <singming9729@gmail.com>
2020-09-09 15:23:59 +0800

    CSM

---
+++ app/src/main/java/com/example/onlinetuition/CreditDebitCard.kt
@@ -0,0 +1,26 @@
+package com.example.onlinetuition
+
+class CreditDebitCard(val paymentId: String?, val name:String, val cardNumber:String, val expiration:String, val ccv:String)
+
+/*
+class CreditDebitCard {
+   */
+/* var paymentId = 1*//*
+
+    var name = ""
+    var cardNumber = 0
+    var expiration = 0
+    var ccv = 0
+
+    constructor(*/
+/*paymentId:Int,*//*
+ name:String, cardNumber:Int, expiration:Int, ccv:Int){
+        */
+/*this.paymentId = paymentId*//*
+
+        this.name = name
+        this.cardNumber = cardNumber
+        this.expiration = expiration
+        this.ccv = ccv
+    }
+}*/
--- app/src/main/java/com/example/onlinetuition/DebitCreditForm.kt
+++ app/src/main/java/com/example/onlinetuition/DebitCreditForm.kt
@@ -1,11 +1,80 @@
 package com.example.onlinetuition
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.FirebaseDatabase
 
 class DebitCreditForm : AppCompatActivity() {
+
+    lateinit var editTextTextPersonName: EditText
+    lateinit var editTextNumberCard: EditText
+    lateinit var editTextDate: EditText
+    lateinit var editTextNumberCCV: EditText
+    lateinit var buttonPay: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_debit_credit_form)
+
+
+        editTextTextPersonName = findViewById(R.id.editTextTextPersonName)
+        editTextNumberCard = findViewById(R.id.editTextNumberCard)
+        editTextDate = findViewById(R.id.editTextDate)
+        editTextNumberCCV = findViewById(R.id.editTextNumberCCV)
+        buttonPay = findViewById(R.id.buttonPay)
+
+        buttonPay.setOnClickListener {
+            saveCreditDebitCard()
+        }
+
+
+/*        var database = FirebaseDatabase .getInstance() .reference
+
+        buttonPay.setOnClickListener {
+            var name = editTextTextPersonName .text.toString()
+            var cardNumber = editTextNumberCard .text.toString().toInt()
+            var expiration = editTextDate .text.toString().toInt()
+            var ccv = editTextNumberCCV .toString().toInt()
+            database.child("payment/").push().setValue(CreditDebitCard (name,cardNumber,expiration,ccv))
+        }*/
     }
+
+        private fun saveCreditDebitCard(){
+            val name = editTextTextPersonName.text.toString().trim()
+            val cardNumber = editTextNumberCard .text.toString().trim()
+            val expiration = editTextDate .text.toString().trim()
+            val ccv = editTextNumberCCV .text.toString().trim()
+            if(name.isEmpty()){
+                editTextTextPersonName.error = "Please enter a name"
+                return
+            }
+            if(cardNumber.isEmpty()){
+                editTextNumberCard.error = "Please enter card number"
+                return
+            }
+            if(expiration.isEmpty()){
+                editTextDate.error = "Please enter card expiration"
+                return
+            }
+            if(ccv.isEmpty()){
+                editTextNumberCCV.error = "Please enter ccv"
+                return
+            }
+
+
+           val ref = FirebaseDatabase.getInstance().getReference("Payment/")
+           val paymentId =  ref.push().key
+
+            val payment = CreditDebitCard(paymentId, name, cardNumber, expiration, ccv)
+
+            ref.child(paymentId.toString()).setValue(payment).addOnCompleteListener {
+                Toast.makeText(applicationContext , "Payment successful", Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+
 }
