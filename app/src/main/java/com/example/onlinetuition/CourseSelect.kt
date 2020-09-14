ba91a78f971953c606d6c5391e285fef9684a78e
Yeawyh <yeawyuheng99@gmail.com>
2020-09-14 16:28:24 +0800

    subject

--- app/src/main/java/com/example/onlinetuition/CourseSelect.kt
+++ app/src/main/java/com/example/onlinetuition/CourseSelect.kt
@@ -1,32 +1,28 @@
 package com.example.onlinetuition
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.util.SparseBooleanArray
 import android.view.View
-import android.widget.*
-import androidx.databinding.DataBindingUtil
-import com.example.onlinetuition.databinding.FragmentTabCourseBinding
-import com.google.firebase.database.*
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.FirebaseDatabase
 
-import kotlinx.android.synthetic.main.activity_course_select.*
 
-class CourseSelect : AppCompatActivity(), AdapterView.OnItemClickListener {
+class CourseSelect : AppCompatActivity() {
 
-    private var listView: ListView? = null
+
+    lateinit var listView: ListView
     private var arrayAdapter: ArrayAdapter<String>? = null
-    lateinit var ref: DatabaseReference
-    lateinit var courseList: MutableList<Course>
-    lateinit var myList: List<String>
-/*    lateinit var listView: ListView*/
-
+    private var subject: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_course_select)
         setSupportActionBar(findViewById(R.id.toolbar_select_course))
-        supportActionBar?.title = "Select Course"
+        supportActionBar?.title = "Select Course (Multiple)"
 
         listView = findViewById(R.id.list_view)
         arrayAdapter = ArrayAdapter(
@@ -35,51 +31,59 @@ class CourseSelect : AppCompatActivity(), AdapterView.OnItemClickListener {
             resources.getStringArray(R.array.subject_item)
         )
 
-        listView?.adapter = arrayAdapter
-        listView?.choiceMode = ListView.CHOICE_MODE_MULTIPLE
-        listView?.onItemClickListener = this
-
-/*    courseList = mutableListOf()
-    ref = FirebaseDatabase.getInstance().getReference("Course")
-    listView = findViewById(R.id.list_view)
-
-
-    val adapter = CourseAdapter(applicationContext, R.layout.activity_course_select, courseList)
-    listView.adapter = adapter*/
+        listView.adapter = arrayAdapter
+        listView.choiceMode = ListView.CHOICE_MODE_MULTIPLE
 
 
         val confirmButton = findViewById<Button>(R.id.confirmButton)
-
         confirmButton.setOnClickListener {
             saveCourse()
+        }
 
 
+        listView.setOnItemClickListener { p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long ->
+            subject += p0?.getItemAtPosition(p2) as String?
 
 
         }
+
+/*        val checked: SparseBooleanArray = listView.getCheckedItemPositions()
+        val size = checked.size() // number of name-value pairs in the array
+
+
+        for (i in 0 until size) {
+            val key = checked.keyAt(i)
+            val value = checked[key]
+            if (value) doSomethingWithSelectedIndex(key)
+        }*/
+
+
 
 
     }
 
     private fun saveCourse() {
-/*        val name = list_view.selectedItemPosition.toString()*/
-
-
-
-
-        val toPayment = Intent(this, Payment::class.java)
-        startActivity(toPayment)
-
-
-    }
-
-    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        val subject: String = parent?.getItemAtPosition(position) as String
         val ref = FirebaseDatabase.getInstance().getReference("User").child("Student")
-        val course = Course(subject)
+        val course = Course("$subject")
 
-        ref.child("5").setValue(course)
+        ref.push().setValue(course)
 
+/*        val toPayment = Intent(this, Payment::class.java)
+intent.putExtra("Total Price",50)
+        startActivity(toPayment)*/
 
     }
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
